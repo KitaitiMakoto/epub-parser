d7fd1d3b5e37482f9b8b964ad6173c77099aaf6f
KITAITI Makoto <KitaitiMakoto@gmail.com>
2011-12-30 04:46:02 +0900

    Add test/test_parser_content_document.rb

---
+++ test/test_parser_content_document.rb
@@ -0,0 +1,18 @@
+require File.expand_path('helper', File.dirname(__FILE__))
+require 'epub/parser/content_document'
+
+class TestParserContentDocument < Test::Unit::TestCase
+  def setup
+    @dir = 'test/fixtures/book'
+    @parser = Parser::ContentDocument.new @dir
+  end
+
+  def test_parse_navigations
+    doc = Nokogiri.XML open("#{@dir}/OPS/nav.xhtml")
+    navs = @parser.parse_navigations doc
+
+    assert_equal 1, navs.length
+    assert_equal 'Table of Contents', navs.first.heading
+  end
+end
+
