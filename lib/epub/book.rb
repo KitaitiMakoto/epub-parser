5fb2e15bb09b2f10813a3a7f3216187184e03d81
KITAITI Makoto <KitaitiMakoto@gmail.com>
2011-12-30 03:19:47 +0900

    Change API

--- lib/epub/book.rb
+++ lib/epub/book.rb
@@ -6,18 +6,24 @@ module EPUB
   class Book
     attr_accessor :ocf, :package, :content_document
 
-    # For enumerator
-    def manifest
+    def each_page_by_spine(&blk)
+      enum = @package.spine.items
+      if block_given?
+        enum.each &blk
+      else
+        enum
+      end
+    end
+    def each_page_by_toc(&blk)
     end
 
-    def each_page_by_spine
-      @package.spine.items
-    end
-
-    def toc
-    end
-
-    def each_content
+    def each_content(&blk)
+      enum = @package.manifest.items
+      if block_given?
+        enum.each &blk
+      else
+        enum.to_enum
+      end
     end
 
     def other_navigation
