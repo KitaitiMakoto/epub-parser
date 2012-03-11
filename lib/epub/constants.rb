7d251a97453e8a4914324701b80423afbae4f45c
KITAITI Makoto <KitaitiMakoto@gmail.com>
2012-03-11 20:45:16 +0900

    Truncate spaces

--- lib/epub/constants.rb
+++ lib/epub/constants.rb
@@ -1,13 +1,13 @@
 module EPUB
   module Constants
     NAMESPACES = {
-      'dc'        => 'http://purl.org/dc/elements/1.1/',
-      'ocf'       => 'urn:oasis:names:tc:opendocument:xmlns:container',
-      'opf'       => 'http://www.idpf.org/2007/opf',
-      'xhtml'     => 'http://www.w3.org/1999/xhtml',
-      'epub'      => 'http://www.idpf.org/2007/ops',
-      'm'         => 'http://www.w3.org/1998/Math/MathML',
-      'svg'       => 'http://www.w3.org/2000/svg'
+      'dc'    => 'http://purl.org/dc/elements/1.1/',
+      'ocf'   => 'urn:oasis:names:tc:opendocument:xmlns:container',
+      'opf'   => 'http://www.idpf.org/2007/opf',
+      'xhtml' => 'http://www.w3.org/1999/xhtml',
+      'epub'  => 'http://www.idpf.org/2007/ops',
+      'm'     => 'http://www.w3.org/1998/Math/MathML',
+      'svg'   => 'http://www.w3.org/2000/svg'
     }
 
     module Type
