006c90d01204399005193076ef54140253dfb281
KITAITI Makoto <KitaitiMakoto@gmail.com>
2012-12-29 21:29:29 +0900

    Make Parser::Utils public

--- lib/epub/parser.rb
+++ lib/epub/parser.rb
@@ -50,6 +50,23 @@ module EPUB
       @book
     end
 
+    module Utils
+      # Extract the value of attribute of element
+      # 
+      # @todo Refinement Nokogiri::XML::Node instead of use this method after Ruby 2.0 becomes popular
+      # 
+      # @param [Nokogiri::XML::Element] element
+      # @param [String] name name of attribute excluding namespace prefix
+      # @param [String, nil] prefix XML namespace prefix in {EPUB::Constants::NAMESPACES} keys
+      # @return [String] value of attribute when the attribute exists
+      # @return nil when the attribute doesn't exist
+      def extract_attribute(element, name, prefix=nil)
+        attr = element.attribute_with_ns(name, EPUB::NAMESPACES[prefix])
+        attr.nil? ? nil : attr.value
+      end
+      module_function :extract_attribute
+    end
+
     private
 
     def create_book(params)
@@ -62,15 +79,6 @@ module EPUB
         require 'epub/book'
         Book.new
       end
-    end
-
-    module Utils
-      # @todo Use refinements instead of this method when Ruby 2.0 becomes popular
-      def extract_attribute(element, name, prefix=nil)
-        attr = element.attribute_with_ns(name, EPUB::NAMESPACES[prefix])
-        attr.nil? ? nil : attr.value
-      end
-      module_function :extract_attribute
     end
   end
 end
