b0d8b9ec4cd2429853349881348fad02f9810fad
KITAITI Makoto <KitaitiMakoto@gmail.com>
2012-12-29 20:29:25 +0900

    Add comments for YARD to EPUB::Parser.parse

--- lib/epub/parser.rb
+++ lib/epub/parser.rb
@@ -1,3 +1,4 @@
+require 'epub'
 require 'epub/constants'
 require 'epub/parser/version'
 require 'epub/parser/ocf'
@@ -9,8 +10,28 @@ require 'nokogiri'
 module EPUB
   class Parser
     class << self
-      def parse(file, options = {})
-        new(file, options).parse
+      # @example
+      #   EPUB::Parser.parse('path/to/book.epub') # => EPUB::Book object
+      # @example
+      #   class MyBook
+      #     include EPUB
+      #   end
+      #   book = MyBook.new
+      #   EPUB::Parser.parse('path/to/book.epub', :book => book) # => #<MyBook:0x000000019760e8 @epub_file=..> (book itself)
+      # @example
+      #   EPUB::Parser.parse('path/to/book.epub', :class => MyBook) # => #<MyBook:0x000000019b0568 @epub_file=...> (the instance of MyClass)
+      # @param [String] filepath
+      # @param [Hash] options the type of return is specified by this argument.
+      #   If no options, returns {EPUB::Book} object.
+      #   For details of options, see below.
+      # @option options [EPUB] :book instance of class which includes {EPUB} module
+      # @option options [Class] :class class which includes {EPUB} module
+      # @return [EPUB] object which is an instance of class including {EPUB} module.
+      #   When option :book passed, returns the same object whose attributes about EPUB are set.
+      #   When option :class passed, returns the instance of the class.
+      #   Otherwise returns {EPUB::Book} object.
+      def parse(filepath, options = {})
+        new(filepath, options).parse
       end
     end
 
