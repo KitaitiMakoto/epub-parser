518d8987af905840be39cbbb47e4f265c51e57c1
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-03-17 06:37:32 +0900

    Implement Parser::ContentDocument#parse_navigations

--- lib/epub/parser/content_document.rb
+++ lib/epub/parser/content_document.rb
@@ -8,9 +8,17 @@ module EPUB
       include Utils
 
       class << self
-        def parse
-          new.parse
+        # @param [EPUB::Publication::Package::Manifest] manifest
+        def parse(manifest)
+          new(manifest).parse
         end
+      end
+
+      attr_reader :manifest
+
+      # @param [EPUB::Publication::Package::Manifest] manifest
+      def initialize(manifest)
+        @manifest = manifest
       end
 
       def parse
@@ -26,11 +34,49 @@ module EPUB
       # @param [Nokogiri::XML::Element] element nav element
       # @return [EPUB::ContentDocument::Navigation::Nav] nav Nav object
       def parse_navigation(element)
-        nav = EPUB::ContentDocument::Navigation::Nav.new
-        nav.heading = find_heading(element)
+        nav = EPUB::ContentDocument::Navigation::Navigation.new
+        nav.text = find_heading(element)
         nav.type = extract_attribute(element, 'type', 'epub')
+        nav.items = element.xpath('./xhtml:ol/xhtml:li', EPUB::NAMESPACES).map {|elem| parse_navigation_item(elem)}
 
         nav
+      end
+
+      # @param [Nokogiri::XML::Element] element li element
+      def parse_navigation_item(element)
+        item = EPUB::ContentDocument::Navigation::Item.new
+        a_or_span = element.xpath('./xhtml:a[1]|xhtml:span[1]', EPUB::NAMESPACES).first
+        return a_or_span if a_or_span.nil?
+
+        item.text = a_or_span.text
+        if a_or_span.name == 'a'
+          if item.text.empty?
+            embedded_content = a_or_span.xpath('./xhtml:audio[1]|xhtml:canvas[1]|xhtml:embed[1]|xhtml:iframe[1]|xhtml:img[1]|xhtml:math[1]|xhtml:object[1]|xhtml:svg[1]|xhtml:video[1]', EPUB::NAMESPACES).first
+            unless embedded_content.nil?
+              case embedded_content.name
+              when 'audio'
+              when 'canvas'
+              when 'embed'
+              when 'iframe'
+                item.text = (extract_attribute(embedded_content, 'name') || extract_attribute(embedded_content, 'srcdoc')).to_s
+              when 'img'
+                item.text = extract_attribute(embedded_content, 'alt').to_s
+              when 'math'
+              when 'object'
+                item.text = extract_attribute(embedded_content, 'name').to_s
+              when 'svg'
+              when 'video'
+              else
+              end
+            end
+            item.text = extract_attribute(a_or_span, 'title').to_s if item.text.nil? || item.text.empty?
+          end
+          item.href = Addressable::URI.parse(extract_attribute(a_or_span, 'href'))
+          item.item = manifest.items.selector {|it| it.href.request_uri == item.href.request_uri}.first
+        end
+        item.items = element.xpath('./xhtml:ol[1]/xhtml:li', EPUB::NAMESPACES).map {|li| parse_navigation_item(li)}
+
+        item
       end
 
       private
--- test/test_parser_content_document.rb
+++ test/test_parser_content_document.rb
@@ -2,16 +2,30 @@ require_relative 'helper'
 
 class TestParserContentDocument < Test::Unit::TestCase
   def setup
+    @manifest = EPUB::Publication::Package::Manifest.new
+    %w[item-1.xhtml item-2.xhtml].each.with_index do |href, index|
+      item = EPUB::Publication::Package::Manifest::Item.new
+      item.id = index
+      item.href = Addressable::URI.parse(href)
+      @manifest << item
+    end
+
     @dir = 'test/fixtures/book'
-    @parser = EPUB::Parser::ContentDocument.new
+    @parser = EPUB::Parser::ContentDocument.new(@manifest)
   end
 
   def test_parse_navigations
     doc = Nokogiri.XML open("#{@dir}/OPS/nav.xhtml")
     navs = @parser.parse_navigations doc
+    nav = navs.first
 
     assert_equal 1, navs.length
-    assert_equal 'Table of Contents', navs.first.heading
-    assert_equal 'toc', navs.first.type
+    assert_equal 'Table of Contents', nav.heading
+    assert_equal 'toc', nav.type
+
+    assert_equal 2, nav.items.length
+    assert_equal @manifest.items.first, nav.items.first.item
+    assert_equal @manifest.items[1], nav.items[1].items[0].item
+    assert_equal @manifest.items[1], nav.items[1].items[1].item
   end
 end
