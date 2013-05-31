45d631ddfeb2c83f813ccb17346fbf200f30e3e7
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-05-31 19:35:13 +0900

    Add tests about inspect

--- test/test_publication.rb
+++ test/test_publication.rb
@@ -132,6 +132,40 @@ class TestPublication < Test::Unit::TestCase
 
       assert_true meta.subexpression?
     end
+
+    def test_dcmes_inspect_includes_class_name
+      meta = Package::Metadata::Title.new
+
+      assert_match /Package::Metadata::Title/, meta.inspect
+    end
+
+    def test_dcmes_inspect_includes_instance_variables
+      meta = Package::Metadata::DCMES.new
+      meta.lang = 'en-US'
+      meta.dir = 'rtl'
+
+      assert_match /@lang/, meta.inspect
+      assert_match /en\-US/, meta.inspect
+      assert_match /@dir/, meta.inspect
+      assert_match /rtl/, meta.inspect
+    end
+
+    def test_meta_inspect_includes_class_name
+      meta = Package::Metadata::Meta.new
+
+      assert_match /Package::Metadata::Meta/, meta.inspect
+    end
+
+    def test_meta_inspect_includes_instance_variables
+      meta = Package::Metadata::Meta.new
+      meta.id = 'meta-id'
+      meta.content = 'meta content'
+
+      assert_match /@id/, meta.inspect
+      assert_match /meta\-id/, meta.inspect
+      assert_match /@content/, meta.inspect
+      assert_match /meta content/, meta.inspect
+    end
   end
 
   class TestManifest < TestPublication
