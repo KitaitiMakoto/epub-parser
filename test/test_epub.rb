b4d393d6e3ae902267961f5338245a9aaa29f4bd
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-05-31 19:23:25 +0900

    Add tests for EPUB#each_page_on_spine

--- test/test_epub.rb
+++ test/test_epub.rb
@@ -12,5 +12,17 @@ class TestEUPB < Test::Unit::TestCase
       book.parse @file
     end
   end
+
+  def test_each_page_on_spine_returns_enumerator_when_block_not_given
+    book = EPUB::Parser.parse(@file)
+    assert_kind_of Enumerator, book.each_page_on_spine
+  end
+
+  def test_enumerator_each_page_on_spine_returns_yields_item
+    enum = EPUB::Parser.parse(@file).each_page_on_spine
+    enum.each do |entry|
+      assert_kind_of EPUB::Publication::Package::Manifest::Item, entry
+    end
+  end
 end
 
