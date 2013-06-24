2f453040cd9a853b8e7afe8703da2cf2888747e2
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-06-24 16:27:03 +0900

    [REFACTORING]Define Guide::Reference::TYPE and use it to define some attribute methods of Guide

--- lib/epub/publication/package/guide.rb
+++ lib/epub/publication/package/guide.rb
@@ -16,18 +16,8 @@ module EPUB
           references << reference
         end
 
-        %w[cover title-page toc index glossary acknowledgements bibliography colophon copyright-page dedication epigraph foreword loi lot notes preface text].each do |type|
-          method_name = type.gsub('-', '_')
-          define_method method_name do
-            var = instance_variable_get "@#{method_name}"
-            return var if var
-
-            var = references.selector {|ref| ref.type == type}.first
-            instance_variable_set "@#{method_name}", var
-          end
-        end
-
         class Reference
+          TYPES = %w[cover title-page toc index glossary acknowledgements bibliography colophon copyright-page dedication epigraph foreword loi lot notes preface text]
           attr_accessor :guide,
                         :type, :title, :href
 
@@ -38,6 +28,17 @@ module EPUB
             @item = @guide.package.manifest.items.selector do |item|
               item.href.request_uri == request_uri
             end.first
+          end
+        end
+
+        Reference::TYPES.each do |type|
+          method_name = type.gsub('-', '_')
+          define_method method_name do
+            var = instance_variable_get "@#{method_name}"
+            return var if var
+
+            var = references.selector {|ref| ref.type == type}.first
+            instance_variable_set "@#{method_name}", var
           end
         end
       end
