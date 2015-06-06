0f013a894dbb11e7a0c08130bb2c4254129deb29
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-06-06 17:27:41 +0900

    Remove EPUB.included method

--- lib/epub.rb
+++ lib/epub.rb
@@ -7,12 +7,3 @@ require 'epub/ocf'
 require 'epub/publication'
 require 'epub/content_document'
 require 'epub/book/features'
-
-module EPUB
-  class << self
-    def included(base)
-      warn 'Including EPUB module is deprecated. Include EPUB::Book::Features instead.'
-      base.__send__ :include, EPUB::Book::Features
-    end
-  end
-end
