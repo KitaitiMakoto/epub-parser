7e6262470afef9a3bc8a5cde43bc5a2178c87c90
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-06-07 22:20:30 +0900

    Add EPUB::Searcher.search method

--- lib/epub/searcher.rb
+++ lib/epub/searcher.rb
@@ -1,3 +1,14 @@
 require 'epub/searcher/result'
 require 'epub/searcher/publication'
 require 'epub/searcher/xhtml'
+
+module EPUB
+  module Searcher
+    class << self
+      # @todo Use named argument in the future
+      def search(epub, word, options={algorithm: :seamless})
+        Publication.search(epub.package, word, options)
+      end
+    end
+  end
+end
