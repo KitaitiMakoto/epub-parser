1fa5f06a14a381ea804f71bed820336932c46084
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-06-14 01:53:11 +0900

    Use OCF::PhysicalContainer in Manifest::Item#read

--- lib/epub/publication/package/manifest.rb
+++ lib/epub/publication/package/manifest.rb
@@ -105,9 +105,8 @@ module EPUB
           end
 
           def read
-            raw_content = Zip::Archive.open(manifest.package.book.epub_file) {|zip|
-              zip.fopen(entry_name) {|member| member.read}
-            }
+            require 'epub/ocf/physical_container' unless OCF.const_defined? :PhysicalContainer
+            raw_content = OCF::PhysicalContainer.read(manifest.package.book.epub_file, entry_name)
 
             unless media_type.start_with?('text/') or
                 media_type.end_with?('xml') or
