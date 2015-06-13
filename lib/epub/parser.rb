2bfe21904d752720c01346ac3689ed83b14392d7
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-06-14 04:36:56 +0900

    Use syntactic sugar

--- lib/epub/parser.rb
+++ lib/epub/parser.rb
@@ -48,7 +48,7 @@ module EPUB
     def parse
       EPUB::OCF::PhysicalContainer.open @filepath do |container|
         @book.ocf = OCF.parse(container)
-        @book.package = Publication.parse(container, @book.ocf.container.rootfile.full_path.to_s)
+        @book.package = Publication.parse(container, @book.rootfile_path)
       end
 
       @book
