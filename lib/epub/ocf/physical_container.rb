17f7d04486efb5022dde69bc21155bc9e8a30e82
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-06-14 01:43:58 +0900

    Define OCF::PhysicalContainer#open and #read

--- lib/epub/ocf/physical_container.rb
+++ lib/epub/ocf/physical_container.rb
@@ -1,8 +1,18 @@
+require 'forwardable'
 require 'epub/ocf/physical_container/zipruby'
 
 module EPUB
   class OCF
     class PhysicalContainer
+      @adapter = Zipruby
+
+      class << self
+        extend Forwardable
+
+        attr_accessor :adapter
+        def_delegators :@adapter, :open, :read
+      end
+
       def initialize(container_path)
         @container_path = container_path
       end
