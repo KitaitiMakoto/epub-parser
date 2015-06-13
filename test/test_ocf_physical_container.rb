a674ee63f2073dda71ff3ff4b0e2e14e9326b73c
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-06-14 02:19:49 +0900

    Assign physical container class to instance variable

--- test/test_ocf_physical_container.rb
+++ test/test_ocf_physical_container.rb
@@ -16,24 +16,25 @@ class TestOCFPhysicalContainer < Test::Unit::TestCase
   class TestZipruby < self
     def setup
       super
-      @container = EPUB::OCF::PhysicalContainer::Zipruby.new(@container_path)
+      @class = EPUB::OCF::PhysicalContainer::Zipruby
+      @container = @class.new(@container_path)
     end
 
     def test_class_method_open
-      EPUB::OCF::PhysicalContainer::Zipruby.open @container_path do |container|
-        assert_instance_of EPUB::OCF::PhysicalContainer::Zipruby, container
+      @class.open @container_path do |container|
+        assert_instance_of @class, container
         assert_equal @content, container.read(@path).force_encoding('UTF-8')
         assert_equal File.read('test/fixtures/book/OPS/日本語.xhtml'), container.read('OPS/日本語.xhtml').force_encoding('UTF-8')
       end
     end
 
     def test_class_method_read
-      assert_equal @content, EPUB::OCF::PhysicalContainer::Zipruby.read(@container_path, @path).force_encoding('UTF-8')
+      assert_equal @content, @class.read(@container_path, @path).force_encoding('UTF-8')
     end
 
     def test_open_yields_over_container_with_opened_archive
       @container.open do |container|
-        assert_instance_of EPUB::OCF::PhysicalContainer::Zipruby, container
+        assert_instance_of @class, container
       end
     end
 
