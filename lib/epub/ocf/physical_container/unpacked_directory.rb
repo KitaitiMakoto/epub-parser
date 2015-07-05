0ba6ebdc1f0553c6b7d23cff3674e156a9ef4641
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-07-05 23:01:10 +0900

    Add UnpackedDirectory file

---
+++ lib/epub/ocf/physical_container/unpacked_directory.rb
@@ -0,0 +1,15 @@
+module EPUB
+  class OCF
+    class PhysicalContainer
+      class UnpackedDirectory < self
+        def open
+          yield self
+        end
+
+        def read(path_name)
+          ::File.read(::File.join(@container_path, path_name))
+        end
+      end
+    end
+  end
+end
