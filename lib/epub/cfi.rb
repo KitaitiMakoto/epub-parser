8bd77de8617bedf1778237934d740f884b09e39d
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-08-09 22:51:21 +0900

    Add CFI classes

---
+++ lib/epub/cfi.rb
@@ -0,0 +1,58 @@
+module EPUB
+  class CFI < Struct.new(:path, :range)
+    class Path < Struct.new(:step, :local_path)
+    end
+
+    class Range < Struct.new(:start, :end)
+    end
+
+    class LocalPath < Struct.new(:steps, :redirected_path, :offset)
+    end
+
+    class RedirectedPath < Struct.new(:offset, :path)
+    end
+
+    class Step < Struct.new(:step, :assertion)
+    end
+
+    class IDAssertion < Struct.new(:id, :parameters)
+    end
+
+    class TextLocationAssertion < Struct.new(:preceded, :followed, :parameters)
+    end
+
+    class CharacterOffset < Struct.new(:offset, :assertion)
+    end
+
+    class SpatialOffset < Struct.new(:x, :y, :temporal, :assertion)
+      def initialize(x, y, temporal, assertion)
+        [x, y].each do |dimension|
+          next unless dimension
+          raise RangeError, "dimension must be in 0..100 but passed #{dimension}" unless (0.0..100.0).cover?(dimension)
+        end
+
+        warn "Assertion is passed to #{__class__} but cannot know how to handle with it: #{assertion}" if assertion
+
+        super
+      end
+
+      def x=(x)
+        raise RangeError, "dimension must be in 0..100 but passed #{x}" unless (0.0..100.0).cover?(x)
+
+        super
+      end
+
+      def y=(y)
+        raise RangeError, "dimension must be in 0..100 but passed #{y}" unless (0.0..100.0).cover?(y)
+
+        super
+      end
+
+      def assertion=(assertion)
+        warn "Assertion is passed to #{__class__} but cannot know how to handle with it: #{assertion}" if assertion
+
+        super
+      end
+    end
+  end
+end
