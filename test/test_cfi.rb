8be7151c5b402a1a0364d87b9357e1025351e995
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-08-11 09:20:47 +0900

    Add test cases for CFI sorting rules

--- test/test_cfi.rb
+++ test/test_cfi.rb
@@ -1,5 +1,6 @@
 require_relative 'helper'
 require 'epub/cfi'
+require 'epub/parser/cfi'
 
 class TestCFI < Test::Unit::TestCase
   def test_escape
@@ -8,5 +9,18 @@ class TestCFI < Test::Unit::TestCase
 
   def test_unescape
     assert_equal '^', EPUB::CFI.unescape('^^')
+  end
+
+  def test_compare
+    assert_compare epubcfi('/6/4[id]'), '<', epubcfi('/6/5')
+    assert_equal epubcfi('/6/4'), epubcfi('/6/4')
+    assert_compare epubcfi('/6/4'), '>', epubcfi('/4/6')
+    assert_compare epubcfi('/6/4!/4@3:7'), '>', epubcfi('/6/4!/4')
+  end
+
+  private
+
+  def epubcfi(string)
+    EPUB::Parser::CFI.new.parse('epubcfi(' + string + ')')
   end
 end
