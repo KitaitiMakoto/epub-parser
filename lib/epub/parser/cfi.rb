8a527eb3a2605d891d02a80117318486947b251f
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-08-11 09:28:41 +0900

    Implement EPUB::Parser::CFI.parse

--- lib/epub/parser/cfi.rb
+++ lib/epub/parser/cfi.rb
@@ -10,6 +10,12 @@ class EPUB::Parser::CFI
   UNICODE_CHARACTER_EXCLUDING_SPECIAL_CHARS_AND_SPACE_AND_DOT_AND_COLON_AND_TILDE_AND_ATMARK_AND_SOLIDUS_AND_EXCLAMATION_MARK_PATTERN = /\u0009|\u000A|\u000D|[\u0022-\u0027]|[\u002A-\u002B]|\u002D|[\u0030-\u0039]|\u003C|[\u003E-\u0040]|[\u0041-\u005A]|\u005C|[\u005F-\u007D]|[\u007F-\uD7FF]|[\uE000-\uFFFD]|[\u10000-\u10FFFF]/ # excluding special chars and space(\u0020) and dot(\u002E) and colon(\u003A) and tilde(\u007E) and atmark(\u0040) and solidus(\u002F) and exclamation mark(\u0021)
   UNICODE_CHARACTER_PATTERN = Regexp.union(UNICODE_CHARACTER_EXCLUDING_SPECIAL_CHARS_AND_SPACE_AND_DOT_AND_COLON_AND_TILDE_AND_ATMARK_AND_SOLIDUS_AND_EXCLAMATION_MARK_PATTERN, Regexp.new(Regexp.escape(EPUB::CFI::SPECIAL_CHARS), / \.:~@!/))
 
+  class << self
+    def parse(string)
+      new.parse(string)
+    end
+  end
+
   def parse(string)
     @scanner = StringScanner.new(string, true)
     @q = []
