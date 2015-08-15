dcee4964ae02f1d3e9f37db172104d5ebb466a59
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-08-16 05:50:13 +0900

    Cover more escaping cases

--- test/test_cfi.rb
+++ test/test_cfi.rb
@@ -6,11 +6,11 @@ require 'nokogiri/diff'
 
 class TestCFI < Test::Unit::TestCase
   def test_escape
-    assert_equal '^^', EPUB::CFI.escape('^')
+    assert_equal '^^^[^]^(^)^,^;^=', EPUB::CFI.escape('^[](),;=')
   end
 
   def test_unescape
-    assert_equal '^', EPUB::CFI.unescape('^^')
+    assert_equal '^[](),;=', EPUB::CFI.unescape('^^^[^]^(^)^,^;^=')
   end
 
   class TestPath < self
