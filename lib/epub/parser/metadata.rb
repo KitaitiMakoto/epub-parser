764c3191b2a48f84f5c4275b3df51d3b8075c1e5
KITAITI Makoto <KitaitiMakoto@gmail.com>
2016-05-22 01:23:50 +0900

    Extract Parser::Metadata from Parser::Publication

---
+++ lib/epub/parser/metadata.rb
@@ -0,0 +1,69 @@
+module EPUB
+  class Parser
+    module Metadata
+      def parse_metadata(doc)
+        metadata = EPUB::Publication::Package::Metadata.new
+        unique_identifier_id = doc.root['unique-identifier']
+        elem = doc.xpath('/opf:package/opf:metadata', EPUB::NAMESPACES).first
+        id_map = {}
+
+        metadata.identifiers = extract_model(elem, id_map, './dc:identifier', :Identifier, ['id']) {|identifier, e|
+          identifier.scheme = extract_attribute(e, 'scheme', 'opf')
+          metadata.unique_identifier = identifier if identifier.id == unique_identifier_id
+        }
+        metadata.titles = extract_model(elem, id_map, './dc:title', :Title)
+        metadata.languages = extract_model(elem, id_map, './dc:language', :DCMES, %w[id])
+        %w[contributor coverage creator date description format publisher relation source subject type].each do |dcmes|
+          metadata.__send__ "#{dcmes}s=", extract_model(elem, id_map, "./dc:#{dcmes}")
+        end
+        metadata.rights = extract_model(elem, id_map, './dc:rights')
+        metadata.metas = extract_refinee(elem, id_map, './opf:meta', :Meta, %w[property id scheme])
+        metadata.links = extract_refinee(elem, id_map, './opf:link', :Link, %w[id media-type]) {|link, e|
+          link.href = extract_attribute(e, 'href')
+          link.rel = Set.new(extract_attribute(e, 'rel').split(nil))
+        }
+
+        id_map.values.each do |hsh|
+          next unless hsh[:refiners]
+          next unless hsh[:metadata]
+          hsh[:refiners].each {|meta| meta.refines = hsh[:metadata]}
+        end
+
+        metadata
+      end
+
+      def extract_model(elem, id_map, xpath, klass=:DCMES, attributes=%w[id lang dir])
+        models = elem.xpath(xpath, EPUB::NAMESPACES).collect do |e|
+          model = EPUB::Publication::Package::Metadata.const_get(klass).new
+          attributes.each do |attr|
+            model.__send__ "#{attr.gsub(/-/, '_')}=", extract_attribute(e, attr)
+          end
+          model.content = e.content unless klass == :Link
+
+          yield model, e if block_given?
+
+          model
+        end
+
+        models.each do |model|
+          id_map[model.id] = {metadata: model} if model.respond_to?(:id) && model.id
+        end
+
+        models
+      end
+
+      def extract_refinee(elem, id_map, xpath, klass, attributes)
+        extract_model(elem, id_map, xpath, klass, attributes) {|model, e|
+          yield model, e if block_given?
+          refines = extract_attribute(e, 'refines')
+          if refines && refines[0] == '#'
+            id = refines[1..-1]
+            id_map[id] ||= {}
+            id_map[id][:refiners] ||= []
+            id_map[id][:refiners] << model
+          end
+        }
+      end
+    end
+  end
+end
