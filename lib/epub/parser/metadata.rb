d4f06b4ed2b796167420ece405f9ec7f6f9fae35
KITAITI Makoto <KitaitiMakoto@gmail.com>
2016-06-06 02:48:28 +0900

    Reimplement Parser::Metadata#parse_metadata to conform Metadata#children requirement

--- lib/epub/parser/metadata.rb
+++ lib/epub/parser/metadata.rb
@@ -5,62 +5,95 @@ module EPUB
         metadata = EPUB::Publication::Package::Metadata.new
         id_map = {}
 
-        metadata.identifiers = extract_model(elem, id_map, './dc:identifier', :Identifier, ['id']) {|identifier, e|
-          identifier.scheme = extract_attribute(e, 'scheme', 'opf')
-          metadata.unique_identifier = identifier if identifier.id == unique_identifier_id
-        }
-        metadata.titles = extract_model(elem, id_map, './dc:title', :Title)
-        metadata.languages = extract_model(elem, id_map, './dc:language', :DCMES, %w[id])
-        %w[contributor coverage creator date description format publisher relation source subject type].each do |dcmes|
-          metadata.__send__ "#{dcmes}s=", extract_model(elem, id_map, "./dc:#{dcmes}")
-        end
-        metadata.rights = extract_model(elem, id_map, './dc:rights')
-        metadata.metas = extract_refinee(elem, id_map, "./#{default_namespace}:meta", :Meta, %w[property id scheme])
-        metadata.links = extract_refinee(elem, id_map, "./#{default_namespace}:link", :Link, %w[id media-type]) {|link, e|
-          link.href = extract_attribute(e, 'href')
-          link.rel = Set.new(extract_attribute(e, 'rel').split(nil))
-        }
+        default_namespace_uri = EPUB::NAMESPACES[default_namespace]
+        elem.element_children.each do |child|
+          namespace_uri = child.namespace && child.namespace.href
+          elem_name = child.name
 
-        id_map.values.each do |hsh|
-          next unless hsh[:refiners]
-          next unless hsh[:metadata]
-          hsh[:refiners].each {|meta| meta.refines = hsh[:metadata]}
-        end
+          model =
+            case namespace_uri
+            when EPUB::NAMESPACES['dc']
+              case elem_name
+              when 'identifier'
+                identifier = build_model(child, :Identifier, ['id'])
+                metadata.identifiers << identifier
+                identifier.scheme = extract_attribute(child, 'scheme', 'opf')
+                identifier
+              when 'title'
+                title = build_model(child, :Title)
+                metadata.titles << title
+                title
+              when 'language'
+                language = build_model(child, :DCMES, ['id'])
+                metadata.languages << language
+                language
+              when 'title', 'contributor', 'coverage', 'creator', 'date', 'description', 'format', 'publisher', 'relation', 'source', 'subject', 'rights', 'type'
+                attr = elem_name == 'rights' ? elem_name : elem_name + 's'
+                dcmes = build_model(child)
+                metadata.__send__(attr) << dcmes
+                dcmes
+              else
+                build_unsupported_model(child)
+              end
+            when default_namespace_uri
+              case elem_name
+              when 'meta'
+                meta = build_model(child, :Meta, %w[property id scheme])
+                metadata.metas << meta
+                meta
+              when 'link'
+                link = build_model(child, :Link, %w[id media-type])
+                metadata.links << link
+                link.href = extract_attribute(child, 'href')
+                link.rel = Set.new(extract_attribute(child, 'rel').split(/\s+/))
+                link
+              else
+                build_unsupported_model(child)
+              end
+            else
+              build_unsupported_model(child)
+            end
 
-        metadata
-      end
+          metadata.children << model
 
-      def extract_model(elem, id_map, xpath, klass=:DCMES, attributes=%w[id lang dir])
-        models = elem.xpath(xpath, EPUB::NAMESPACES).collect do |e|
-          model = EPUB::Publication::Package::Metadata.const_get(klass).new
-          attributes.each do |attr|
-            model.__send__ "#{attr.gsub(/-/, '_')}=", extract_attribute(e, attr)
+          if model.kind_of?(EPUB::Metadata::Identifier) &&
+             model.id == unique_identifier_id
+            metadata.unique_identifier = model
           end
-          model.content = e.content unless klass == :Link
 
-          yield model, e if block_given?
+          if model.respond_to?(:id) && model.id
+            id_map[model.id] = {refinee: model}
+          end
 
-          model
-        end
-
-        models.each do |model|
-          id_map[model.id] = {metadata: model} if model.respond_to?(:id) && model.id
-        end
-
-        models
-      end
-
-      def extract_refinee(elem, id_map, xpath, klass, attributes)
-        extract_model(elem, id_map, xpath, klass, attributes) {|model, e|
-          yield model, e if block_given?
-          refines = extract_attribute(e, 'refines')
-          if refines && refines[0] == '#'
+          refines = extract_attribute(child, 'refines')
+          if refines && refines.start_with?('#')
             id = refines[1..-1]
             id_map[id] ||= {}
             id_map[id][:refiners] ||= []
             id_map[id][:refiners] << model
           end
-        }
+        end
+
+        id_map.values.each do |hsh|
+          next unless hsh[:refiners]
+          next unless hsh[:refinee]
+          hsh[:refiners].each {|meta| meta.refines = hsh[:refinee]}
+        end
+
+        metadata
+      end
+
+      def build_model(elem, klass=:DCMES, attributes=%w[id lang dir])
+        model = EPUB::Metadata.const_get(klass).new
+        attributes.each do |attr|
+          model.__send__ "#{attr.gsub('-', '_')}=", extract_attribute(elem, attr)
+        end
+        model.content = elem.content unless klass == :Link
+        model
+      end
+
+      def build_unsupported_model(elem)
+        EPUB::Metadata::UnsupportedModel.new(elem)
       end
     end
   end
