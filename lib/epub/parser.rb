ce4bc256f4c23ebdfbd8884753ad7c736afe90f9
Taylor Scott <tscott@deseretbook.com>
2016-07-12 14:32:07 -0600

    allow class including EPUB to intialize with extra arguments

--- lib/epub/parser.rb
+++ lib/epub/parser.rb
@@ -7,10 +7,10 @@ module EPUB
   class Parser
     class << self
       # Parse an EPUB file
-      # 
+      #
       # @example
       #   EPUB::Parser.parse('path/to/book.epub') # => EPUB::Book object
-      # 
+      #
       # @example
       #   class MyBook
       #     include EPUB
@@ -18,11 +18,11 @@ module EPUB
       #   book = MyBook.new
       #   parsed_book = EPUB::Parser.parse('path/to/book.epub', :book => book) # => #<MyBook:0x000000019760e8 @epub_file=..>
       #   parsed_book.equal? book # => true
-      # 
+      #
       # @example
       #   book = EPUB::Parser.parse('path/to/book.epub', :class => MyBook) # => #<MyBook:0x000000019b0568 @epub_file=...>
       #   book.instance_of? MyBook # => true
-      # 
+      #
       # @param [String] filepath
       # @param [Hash] options the type of return is specified by this argument.
       #   If no options, returns {EPUB::Book} object.
@@ -81,7 +81,11 @@ module EPUB
       when params[:book]
         params[:book]
       when params[:class]
-        params[:class].new
+        if params[:initialize_with]
+          params[:class].new params[:initialize_with]
+        else
+          params[:class].new
+        end
       else
         Book.new
       end
