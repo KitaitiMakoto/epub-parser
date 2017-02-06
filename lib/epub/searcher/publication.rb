e0959e200ff0ee01a85ca01aa5472e1fc918cb8b
KITAITI Makoto <KitaitiMakoto@gmail.com>
2017-02-06 23:07:19 +0900

    Implement #search_element

--- lib/epub/searcher/publication.rb
+++ lib/epub/searcher/publication.rb
@@ -7,6 +7,10 @@ module EPUB
         def search_text(package, word, **options)
           new(package).search_text(word, options)
         end
+
+        def search_element(package, css: nil, xpath: nil, namespaces: {})
+          new(package).search_element(css: css, xpath: xpath, namespaces: namespaces)
+        end
       end
 
       def initialize(package)
@@ -26,6 +30,49 @@ module EPUB
         end
 
         results
+      end
+
+      # @todo: Refactoring
+      # @todo: Accepst namespaces for XPath
+      def search_element(css: nil, xpath: nil, namespaces: {})
+        raise ArgumentError, 'Both css and xpath are nil' if css.nil? && xpath.nil?
+
+        namespaces = EPUB::NAMESPACES.merge(namespaces)
+        results = []
+
+        spine_step = EPUB::CFI::Step.new(EPUB::Publication::Package::CONTENT_MODELS.index(:spine) * 2)
+        @package.spine.each_itemref.with_index do |itemref, index|
+          assertion = itemref.id ? EPUB::CFI::IDAssertion.new(itemref.id) : nil
+          itemref_step = EPUB::CFI::Step.new((index + 1) * 2, assertion)
+          path_to_itemref = EPUB::CFI::Path.new([spine_step, itemref_step])
+          content_document = itemref.item.content_document
+          next unless content_document
+          doc = content_document.nokogiri
+          elems = if xpath
+                    doc.xpath(xpath, namespaces)
+                  else
+                    doc.css(css)
+                  end
+          elems.each do |elem|
+            path = find_path(elem)
+            results << EPUB::CFI::Location.new([path_to_itemref, path])
+          end
+        end
+
+        results
+      end
+
+      private
+
+      def find_path(elem)
+        steps = []
+        until elem.parent.document?
+          index = elem.parent.element_children.index(elem)
+          assertion = elem["id"] ? EPUB::CFI::IDAssertion.new(elem["id"]) : nil
+          steps.unshift EPUB::CFI::Step.new((index + 1) * 2, assertion)
+          elem = elem.parent
+        end
+        EPUB::CFI::Path.new(steps)
       end
     end
   end
