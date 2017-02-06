a8e83887a52b06c9bde37ef62caca3b318981739
KITAITI Makoto <KitaitiMakoto@gmail.com>
2017-02-06 14:02:58 +0900

    Make Searcher#initialize accept package and #search accept word

--- lib/epub/searcher/publication.rb
+++ lib/epub/searcher/publication.rb
@@ -5,22 +5,22 @@ module EPUB
     class Publication
       class << self
         def search_text(package, word, **options)
-          new(word).search_text(package, options)
+          new(package).search_text(word, options)
         end
       end
 
-      def initialize(word)
-        @word = word
+      def initialize(package)
+        @package = package
       end
 
-      def search_text(package, algorithm: :seamless)
+      def search_text(word, algorithm: :seamless)
         results = []
 
-        spine = package.spine
+        spine = @package.spine
         spine_step = Result::Step.new(:element, 2, {:name => 'spine', :id => spine.id})
         spine.each_itemref.with_index do |itemref, index|
           itemref_step = Result::Step.new(:itemref, index, {:id => itemref.id})
-          XHTML::ALGORITHMS[algorithm].search_text(Nokogiri.XML(itemref.item.read), @word).each do |sub_result|
+          XHTML::ALGORITHMS[algorithm].search_text(Nokogiri.XML(itemref.item.read), word).each do |sub_result|
             results << Result.new([spine_step, itemref_step] + sub_result.parent_steps, sub_result.start_steps, sub_result.end_steps)
           end
         end
