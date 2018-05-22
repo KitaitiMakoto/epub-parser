c9b6b1393d3f22c77bc03f28a557b91c36287931
KITAITI Makoto <KitaitiMakoto@gmail.com>
2018-05-22 22:14:29 +0900

    Add EPUB::Parser::NokogiriAttributeWithPrefix refinement

--- lib/epub/parser/utils.rb
+++ lib/epub/parser/utils.rb
@@ -1,5 +1,15 @@
+require "nokogiri"
+
 module EPUB
   class Parser
+    module NokogiriAttributeWithPrefix
+      refine Nokogiri::XML::Node do
+        def attribute_with_prefix(name, prefix = nil)
+          attribute_with_ns(name, EPUB::NAMESPACES[prefix])&.value
+        end
+      end
+    end
+
     module Utils
       # Extract the value of attribute of element
       # 
