db14e60dc11ad1937fec3df21a2b12edc1b69983
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2018-10-29 23:58:22 +0900

    Fix document comments for introducing Parser::XMLDocument

--- lib/epub/searcher/publication.rb
+++ lib/epub/searcher/publication.rb
@@ -39,7 +39,7 @@ module EPUB
 
       # @todo: Refactoring
       # @return [Array<Hash>] An array of rearch results. Each result is composed of:
-      #   :element: [Nokogiri::XML::ELement] Found element
+      #   :element: [REXML::Element, Nokogiri::XML::ELement] Found element
       #   :itemref: [EPUB::Publication::Package::Spine::Itemref] Itemref that element's document belongs to
       #   :location: [EPUB::CFI::Location] CFI that indicates the element
       #   :package: [EPUB::Publication::Package] Package that the element belongs to
