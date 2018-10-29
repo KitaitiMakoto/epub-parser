9b06a1c25ca759707483b79f20e8ef486ecb7125
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2018-10-29 21:06:24 +0900

    Move XML refinements to parser/xml_document.rb from parser/utils.rb

--- lib/epub/parser/xml_document.rb
+++ lib/epub/parser/xml_document.rb
@@ -1,4 +1,5 @@
-require "epub/parser/utils"
+require "rexml/document"
+require "nokogiri"
 
 module EPUB
   class Parser
@@ -6,6 +7,26 @@ module EPUB
       class << self
         def new(xml)
           REXML::Document.new(xml)
+        end
+      end
+    end
+
+    module REXMLRefinements
+      refine REXML::Element do
+        def each_element_by_xpath(xpath, namespaces = nil, &block)
+          REXML::XPath.each self, xpath, namespaces, &block
+        end
+
+        def attribute_with_prefix(name, prefix = nil)
+          attribute(name, EPUB::NAMESPACES[prefix])&.value
+        end
+      end
+    end
+
+    module NokogiriAttributeWithPrefix
+      refine Nokogiri::XML::Node do
+        def attribute_with_prefix(name, prefix = nil)
+          attribute_with_ns(name, EPUB::NAMESPACES[prefix])&.value
         end
       end
     end
