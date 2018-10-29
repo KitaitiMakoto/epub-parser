40ce5361dc126d8a3d67a2850585c4665b00b919
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2018-10-29 22:08:57 +0900

    Define Nokogiri::XML::Node#each_element_by_xpath

--- lib/epub/parser/xml_document.rb
+++ lib/epub/parser/xml_document.rb
@@ -24,6 +24,10 @@ module EPUB
         end
 
         refine Nokogiri::XML::Node do
+          def each_element_by_xpath(xpath, namespaces = nil, &block)
+            xpath(xpath, namespaces).each &block
+          end
+
           def attribute_with_prefix(name, prefix = nil)
             attribute_with_ns(name, EPUB::NAMESPACES[prefix])&.value
           end
