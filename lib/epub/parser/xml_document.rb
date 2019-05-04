f0599ea43f949aee9f33ec64f08a35bf1c5157ec
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2019-05-05 04:57:00 +0900

    Remove parser/xml_document/refinements.rb

--- lib/epub/parser/xml_document.rb
+++ lib/epub/parser/xml_document.rb
@@ -16,4 +16,10 @@ module EPUB
   end
 end
 
-require "epub/parser/xml_document/refinements"
+require "epub/parser/xml_document/refinements/rexml"
+begin
+  require "epub/parser/xml_document/refinements/nokogiri"
+  EPUB::Parser::XMLDocument.backend = :Nokogiri
+rescue LoadError
+  EPUB::Parser::XMLDocument.backend = :REXML
+end
--- lib/epub/parser/xml_document/refinements.rb
+++
@@ -1,7 +0,0 @@
-require "epub/parser/xml_document/refinements/rexml"
-begin
-  require "epub/parser/xml_document/refinements/nokogiri"
-  EPUB::Parser::XMLDocument.backend = :Nokogiri
-rescue LoadError
-  EPUB::Parser::XMLDocument.backend = :REXML
-end
