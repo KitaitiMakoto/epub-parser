65590b80dd417fb5ff5e9e379fb48d92142bcb9a
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2023-12-29 00:50:37 +0900

    Move test's ConcreteContainer module to helper file

--- test/helper.rb
+++ test/helper.rb
@@ -18,3 +18,33 @@ require 'epub/parser'
 if ENV["XML_BACKEND"]
   EPUB::Parser::XMLDocument.backend = ENV["XML_BACKEND"].to_sym
 end
+
+module ConcreteContainer
+  def test_class_method_open
+    @class.open @container_path do |container|
+      assert_instance_of @class, container
+      assert_equal @content, container.read(@path).force_encoding('UTF-8')
+      assert_equal File.read('test/fixtures/book/OPS/日本語.xhtml'), container.read('OPS/日本語.xhtml').force_encoding('UTF-8')
+    end
+  end
+
+  def test_class_method_read
+    assert_equal @content, @class.read(@container_path, @path).force_encoding('UTF-8')
+  end
+
+  def test_open_yields_over_container_with_opened_archive
+    @container.open do |container|
+      assert_instance_of @class, container
+    end
+  end
+
+  def test_container_in_open_block_can_readable
+    @container.open do |container|
+      assert_equal @content, container.read(@path).force_encoding('UTF-8')
+    end
+  end
+
+  def test_read
+    assert_equal @content, @container.read(@path).force_encoding('UTF-8')
+  end
+end
--- test/test_ocf_physical_container.rb
+++ test/test_ocf_physical_container.rb
@@ -13,36 +13,6 @@ class TestOCFPhysicalContainer < Test::Unit::TestCase
     assert_equal @content, EPUB::OCF::PhysicalContainer.read(@container_path, @path).force_encoding('UTF-8')
   end
 
-  module ConcreteContainer
-    def test_class_method_open
-      @class.open @container_path do |container|
-        assert_instance_of @class, container
-        assert_equal @content, container.read(@path).force_encoding('UTF-8')
-        assert_equal File.read('test/fixtures/book/OPS/日本語.xhtml'), container.read('OPS/日本語.xhtml').force_encoding('UTF-8')
-      end
-    end
-
-    def test_class_method_read
-      assert_equal @content, @class.read(@container_path, @path).force_encoding('UTF-8')
-    end
-
-    def test_open_yields_over_container_with_opened_archive
-      @container.open do |container|
-        assert_instance_of @class, container
-      end
-    end
-
-    def test_container_in_open_block_can_readable
-      @container.open do |container|
-        assert_equal @content, container.read(@path).force_encoding('UTF-8')
-      end
-    end
-
-    def test_read
-      assert_equal @content, @container.read(@path).force_encoding('UTF-8')
-    end
-  end
-
   begin
     require 'epub/ocf/physical_container/zipruby'
     class TestZipruby < self
